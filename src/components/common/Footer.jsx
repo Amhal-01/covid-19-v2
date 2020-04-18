eb6327f9bf5150567354dbb77b9fcbff7fe6a359
Amhal Hamza <amhalhamza@gmail.com>
2020-04-18 23:38:59 +0100

    HA: Adding Footer,Loader,App extension from JS to JSX.

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="page-footer">
+      <div className="container-fluid text-center text-md-left">
+        <div className="footer-copyright text-center">
+          © 2020 Copyright: <b>Made by Hamza</b>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
